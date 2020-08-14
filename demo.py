260b584f5915134383192a8c7bc1c88b0fbd0b2d
Ubuntu <devops@knode.zpyygd2mc5renb5ifx4rupyv3h.dx.internal.cloudapp.net>
2020-08-14 11:59:19 +0000

    Modified for K8S Integrations

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Docker Flask Demo"
+    return "Welcome to Flask App for K8S Integration"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)

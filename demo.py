69b86c48f663365e42b133c403958af7fd60e3c0
Ubuntu <devops@knode.zpyygd2mc5renb5ifx4rupyv3h.dx.internal.cloudapp.net>
2020-08-17 13:00:38 +0000

    Git poller update with only kubectl CLI

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask App Triggered using Git Pooler"
+    return "Welcome to Flask App Triggered using Git Pooler"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
